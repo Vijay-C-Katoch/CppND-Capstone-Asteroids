68a4a866c9e48a3ceea41b4afba93d58d9242518
Vijay Katoch <katoch.vijay@gmail.com>
2021-10-22 22:38:18 +0200

    Adding Math for 2D and algebra

--- src/math/ndLinAlgebra.h
+++ src/math/ndLinAlgebra.h
@@ -5,12 +5,17 @@
 #include <vector>
 #include <cmath>
 #include <initializer_list>
+#include <algorithm>
+#include <numeric>
 
 #include "ndException.h"
 
 namespace nd
 {
 
+    /* ****************************************************************
+    *  Vectors 
+    /* ****************************************************************/
     /// N dimension vector
     template <class T>
     class ndVector
@@ -23,7 +28,7 @@ namespace nd
 
         ndVector(std::vector<T> inVector);
 
-        ~ndVector();
+        ~ndVector() = default;
 
         std::size_t NumDims() const;
 
@@ -31,11 +36,11 @@ namespace nd
 
         T GetAtIndex(std::size_t idx);
 
-        T Length();
+        T Norm();
 
         void Normalize();
 
-        ndVector<T>GetNormalized();
+        ndVector<T>Normalized();
 
         // overloads
         ndVector<T> operator+ (const ndVector<T>& rhs) const;
@@ -51,14 +56,63 @@ namespace nd
         //cross product, identify plane of vectors
         static ndVector<T> cross(const ndVector<T>& v1, const ndVector<T>& v2);
 
-
-
-
     private:
         std::vector<T> _vector;
         std::size_t _nDims;
 
     };
+
+    template <class T>
+    ndVector<T>::ndVector(std::size_t numDims) : _nDims{numDims}
+    {
+        _vector = std::vector<T>(numDims, static_cast<T>(0));
+    }
+
+    template <class T>
+    ndVector<T>::ndVector(std::initializer_list<T> inList)
+    {
+        const T* it = inList.begin();
+        const T* const end = inList.end();
+
+        for (; it != end; ++it)
+            _vector.push_back(*it);
+    }
+
+    template <class T>
+    ndVector<T>::ndVector(std::vector<T> inVector)
+    {
+        _nDims = inVector.size();
+        _vector = inVector;
+    }
+
+    template <class T>
+    std::size_t ndVector<T>::NumDims() const
+    {
+        return _nDims;
+    }
+
+    template <class T>
+    void ndVector<T>::SetAtIndex(std::size_t idx, T val)
+    {
+        _vector[idx] = val;
+    }
+
+    template <class T>
+    T ndVector<T>::GetAtIndex(std::size_t idx)
+    {
+        return _vector[idx];
+    }
+
+    template <class T>
+    T ndVector<T>::Norm()
+    {
+        T sqrdSum = std::accumulate(_vector.begin(), _vector.end(), decltype(_vector)::value_type(0), [](T a, T b) 
+        {
+            return a + b * b;
+        });
+
+    }
+
 }
 
 #endif /* ND_LIN_ALGEBRA_H_ */
